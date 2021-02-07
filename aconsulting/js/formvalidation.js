6ca9e882f0c18bd51ec605ea4ff9797e9a27009e
Demqq <73760785+Demqq@users.noreply.github.com>
2021-02-07 21:00:05 +0100

    done

---
+++ aconsulting/js/formvalidation.js
@@ -0,0 +1,18 @@
+let form = document.querySelector('form');
+
+let nameInput = document.querySelector('#name');
+let emailInput = document.querySelector('#email');
+let submitInput = document.querySelector('#submit');
+let timeInput = document.querySelector('#time');
+
+submitInput.addEventListener('click', function(){
+    if (emailInput.value.indexOf("@") == -1 ) {
+        alert("Please, write down you Name or correct E-mail address")
+    } else if (nameInput.value.length === 0) {
+        alert("Write, please, correct name");
+    } else if (timeInput.value == ""){
+        alert("Choose the time");
+    } else {
+        alert("Thank you, your appointment was being booked");
+    }
+    });
